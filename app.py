b6067d192d80c3415f936e01bba4a619cb0c35c0
vietthanh <vietthanh19762004@gmail.com>
2025-11-27 10:40:00 +0700

    cập nhập app.py

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def create_speech_client_from_secrets():
         
     try:
         credentials_dict = json.loads(GCP_CREDENTIALS_JSON)
+        print("nội dung json:\n", credentials_dict)
         
         # Tạo file tạm thời (Google Cloud Client Libraries yêu cầu đường dẫn file)
         temp_file_path = ""
